f7880e943848a8da4a9bd642b29c12fdcca2cc88
bfker <bfker@qq.com>
2023-10-31 21:03:54 +0800

    modify login action

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -35,7 +35,7 @@ const actions = {
     return new Promise((resolve, reject) => {
       login({ username: username.trim(), password: password }).then(response => {
         const { data } = response.data
-        console.log(data)
+        console.log(response)
         commit('SET_TOKEN', data.token)
         setToken(data.token)
         resolve()
