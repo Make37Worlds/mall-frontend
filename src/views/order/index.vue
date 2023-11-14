91d11fc3a06c4df9ff2d2f387f43c2bf185f8465
bfker <bfker@qq.com>
2023-11-14 20:10:01 +0800

    modify buy product, id->productId

--- src/views/order/index.vue
+++ src/views/order/index.vue
@@ -20,8 +20,8 @@
       @close="resetBuyProductForm"
     >
       <el-form :model="newProductForm" label-width="80px">
-        <el-form-item label="ID" prop="id">
-          <el-input v-model="newProductForm.id" />
+        <el-form-item label="ID" prop="productId">
+          <el-input v-model="newProductForm.productId" />
         </el-form-item>
         <el-form-item label="Quantity" prop="quantity">
           <el-input v-model="newProductForm.quantity" />
@@ -46,7 +46,7 @@ export default {
     return {
       products: [],
       newProductForm: {
-        id: '',
+        productId: '',
         quantity: null
       },
       buyProductDialogVisible: false
@@ -73,11 +73,11 @@ export default {
     },
     buyProduct() {
       console.log('token:', this.$store.getters.token)
+      console.log('token:', this.newProductForm)
       // 向产品数组中添加新产品
       // 在这里将新产品数据发送到后端/API
-      const { id, quantity } = this.newProductForm
       return new Promise((resolve, reject) => {
-        buyProduct({ id: id.trim(), quantity: quantity }).then(response => {
+        buyProduct({ productId: this.newProductForm.productId, quantity: this.newProductForm.quantity }).then(response => {
           console.log(response)
           this.resetBuyProductForm()
           this.buyProductDialogVisible = false
@@ -90,7 +90,7 @@ export default {
     resetBuyProductForm() {
       // 重置新产品表单
       this.newProductForm = {
-        id: '',
+        productId: '',
         quantity: null
       }
     }
